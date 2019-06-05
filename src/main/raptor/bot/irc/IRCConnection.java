e5d0cdcece9f5938fd3291d24b9a1c035f5d2f65
raptor0719 <shortman_gamer@hotmail.com>
2019-06-05 00:28:07 -0400

    Start implementing the irc connection

---
+++ src/main/raptor/bot/irc/IRCConnection.java
@@ -0,0 +1,42 @@
+package raptor.bot.irc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class IRCConnection {
+	private final Socket tcpConnection;
+	private final BufferedReader serverMessageStream;
+	private final BufferedWriter clientMessageStream;
+
+	public IRCConnection(final String address, final int port) throws UnknownHostException, IOException {
+		tcpConnection = new Socket(address, port);
+		serverMessageStream = new BufferedReader(new InputStreamReader(tcpConnection.getInputStream()));
+		clientMessageStream = new BufferedWriter(new OutputStreamWriter(tcpConnection.getOutputStream()));
+	}
+
+	public void user(final String username, final String realName) {
+		sendMessage("USER %s * * :%s\n", username, realName);
+	}
+
+	public void nick(final String nickname) {
+		sendMessage("NICK %s\n", nickname);
+	}
+
+	public void pong(final String pingVal) {
+		sendMessage("PONG :%s\n", pingVal);
+	}
+
+	private void sendMessage(final String commandFormat, final Object... args) {
+		try {
+			clientMessageStream.write(String.format(commandFormat, args));
+			clientMessageStream.flush();
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
