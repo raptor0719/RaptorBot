fdbf9c24cd16e36e0a6b33f7024dc536c235a899
raptor0719 <shortman_gamer@hotmail.com>
2019-11-12 06:20:59 -0500

    Add bot command parser test

--- src/test/raptor/bot/command/BotCommandParserTest.java
+++ src/test/raptor/bot/command/BotCommandParserTest.java
@@ -8,11 +8,18 @@ import raptor.bot.command.commands.SoundCommand;
 
 public class BotCommandParserTest {
 	@Test
-	public void test() {
+	public void soundCommand_correct() {
 		final String message = "!sound ynot";
 		final BotCommand command = BotCommandParser.parseBotCommand(message);
 		Assert.assertEquals("sound", command.getCommand());
 		final SoundCommand soundCommand = (SoundCommand)command;
 		Assert.assertEquals("ynot", soundCommand.getSound());
+	}
+
+	@Test
+	public void aliasCommand_correct() {
+		final String message = "!ynot";
+		final BotCommand command = BotCommandParser.parseBotCommand(message);
+		Assert.assertEquals("ynot", command.getCommand());
 	}
 }
