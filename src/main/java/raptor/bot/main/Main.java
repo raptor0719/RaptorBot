5c05500d501606b8647b22f6823caf8797ed921d
raptor0719 <shortman_gamer@hotmail.com>
2019-11-22 10:14:41 -0500

    Read sound mappings from an external file
    Begin making the bot configurable by externalizing the sound mappings

--- src/main/java/raptor/bot/main/Main.java
+++ src/main/java/raptor/bot/main/Main.java
@@ -28,61 +28,14 @@ import raptor.bot.utils.words.PartOfSpeech;
 import raptor.bot.utils.words.WordBank;
 
 public class Main {
-	private static final Map<String, String> sounds;
+	private static final String soundsFile = "C:\\Users\\short\\Documents\\RaptorBot\\sounds\\sounds.properties";
 	private static final String aliasFile = "C:\\Users\\short\\Documents\\RaptorBot\\aliases.txt";
 	private static final String wordBankFile = "C:\\Users\\short\\Documents\\GitHub\\RaptorBot\\src\\main\\resources\\dictionary.txt";
-
-	static {
-		final String soundDir = "C:\\Users\\short\\Documents\\RaptorBot\\sounds\\";
-		sounds = new HashMap<String, String>();
-
-		sounds.put("ynot", soundDir + "WC3-Peon-WhyNot.wav");
-		sounds.put("imdead", soundDir + "WC3-Peasant-ImDead.wav");
-		sounds.put("nice", soundDir + "WC3-Grunt-KindOfNice.wav");
-		sounds.put("death", soundDir + "WC3-BloodMage-Dead.wav");
-		sounds.put("engineer", soundDir + "WC3-BelfEngineer-ImAnEngineer.wav");
-		sounds.put("wanderer", soundDir + "WC3-Bandit-RoamerWanderer.wav");
-		sounds.put("jobsdone", soundDir + "WC3-Peasant-JobsDone.wav");
-		sounds.put("taunt", soundDir + "WC3-Muradin-BestYouCanDo.wav");
-
-		sounds.put("moredots", soundDir + "OWA-MoreDotsMoreDots.wav");
-		sounds.put("dotsnow", soundDir + "OWA-MoreDotsNow.wav");
-		sounds.put("stopdots", soundDir + "OWA-StopDots.wav");
-		sounds.put("whodafuq", soundDir + "OWA-WhoTheWasThat.wav");
-		sounds.put("wtf", soundDir + "OWA-WTF.wav");
-		sounds.put("wtfshit", soundDir + "OWA-WTFShit.wav");
-		sounds.put("cmere", soundDir + "OWA-ComeHere.wav");
-		sounds.put("eff", soundDir + "OWA-Fuck.wav");
-
-		sounds.put("godlike", soundDir + "DOTA-Godlike.wav");
-		sounds.put("holyshit", soundDir + "DOTA-HolyShit.wav");
-		sounds.put("monsterkill", soundDir + "DOTA-MonsterKill.wav");
-		sounds.put("ultrakill", soundDir + "DOTA-UltraKill.wav");
-
-		sounds.put("chickens", soundDir + "MYTH2-ChickensAttack.wav");
-		sounds.put("doing", soundDir + "MYTH2-Dwarf-DOOOIIINNNGGG.wav");
-		sounds.put("weedoggie", soundDir + "MYTH2-Dwarf-WeeDoggie.wav");
-		sounds.put("casualty", soundDir + "MYTH2-Casualty.wav");
-
-		sounds.put("321", soundDir + "TS3-321GO.wav");
-		sounds.put("usuck", soundDir + "TS3-YouSuck.wav");
-
-		sounds.put("ugliness", soundDir + "SPONGEBOB-UglyDistraction.wav");
-		sounds.put("ouch", soundDir + "SPONGEBOB-Ouch.wav");
-		sounds.put("rectangles", soundDir + "SPONGEBOB-Rectangles.wav");
-
-		sounds.put("casual", soundDir + "Casual.wav");
-		sounds.put("freshmeat", soundDir + "DIABLO1-FreshMeat.wav");
-		sounds.put("hax", soundDir + "IdiotBox-Hax.wav");
-		sounds.put("victory", soundDir + "FF-Victory.wav");
-		sounds.put("fatality", soundDir + "MK-Fatality.wav");
-		sounds.put("herb", soundDir + "AOE2-HerbLaugh.wav");
-	}
 
 	public static void main(String[] args) {
 		extractWords(wordBankFile);
 		if (args.length >= 1 && Boolean.parseBoolean(args[0])) {
-			new TestWindow(new RaptorBot(new SoundManager(sounds), new AliasManager(aliasFile), getChatProcessor(), new MadlibManager(getWordBank(wordBankFile))));
+			new TestWindow(new RaptorBot(new SoundManager(soundsFile), new AliasManager(aliasFile), getChatProcessor(), new MadlibManager(getWordBank(wordBankFile))));
 			return;
 		}
 
@@ -96,7 +49,7 @@ public class Main {
 		final String password = getOathToken();
 //		final IRCClient client = new IRCClient(ip, port, user, nick);
 		final IRCClient client = new IRCClient(ip, port, user, nick, password);
-		final RaptorBot bot = new RaptorBot(new SoundManager(sounds), new AliasManager(aliasFile), getChatProcessor(), new MadlibManager(getWordBank(wordBankFile)));
+		final RaptorBot bot = new RaptorBot(new SoundManager(soundsFile), new AliasManager(aliasFile), getChatProcessor(), new MadlibManager(getWordBank(wordBankFile)));
 
 		final long messageDelay = 1000L;
 		long lastMessageTime = 0L;
--- src/main/java/raptor/bot/utils/SoundManager.java
+++ src/main/java/raptor/bot/utils/SoundManager.java
@@ -4,15 +4,21 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import raptor.bot.api.ISoundManager;
 
 public class SoundManager implements ISoundManager<String> {
 	private final Map<String, String> sounds;
 
-	public SoundManager(final Map<String, String> sounds) {
-		this.sounds = sounds;
+	public SoundManager(final String soundsConfigPath) {
+		this.sounds = readSoundProperties(soundsConfigPath);
+		verifySoundsExist();
 	}
 
 	@Override
@@ -37,5 +43,47 @@ public class SoundManager implements ISoundManager<String> {
 	@Override
 	public Iterable<String> getKeys() {
 		return sounds.keySet();
+	}
+
+	private Map<String, String> readSoundProperties(final String path) {
+		final File soundPropsFile = new File(path);
+		final String parentPath = soundPropsFile.getParentFile().getAbsolutePath();
+
+		FileInputStream fis = null;
+		try {
+			final Properties soundProps = new Properties();
+
+			fis = new FileInputStream(soundPropsFile);
+			soundProps.load(fis);
+
+			final Map<String, String> soundsMap = new HashMap<String, String>();
+			for (final Map.Entry<Object, Object> e : soundProps.entrySet())
+				soundsMap.put((String)e.getKey(), Paths.get(parentPath, (String)e.getValue()).toString());
+
+			return soundsMap;
+		} catch (Throwable t) {
+			throw new RuntimeException(t);
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (Throwable t) {/* IGNORE FAILURES */}
+		}
+	}
+
+	private boolean verifySoundsExist() {
+		boolean allExist = true;
+		final List<String> keysToRemove = new ArrayList<>();
+		for (final Map.Entry<String, String> e : sounds.entrySet()) {
+			final File soundFile = new File(e.getValue());
+			if (!soundFile.exists() || soundFile.isDirectory()) {
+				allExist = false;
+				keysToRemove.add(e.getKey());
+				System.err.println(String.format("The '%s' sound's file '%s' was not found. It is being removed from the list of available sounds.", e.getKey(), e.getValue()));
+			}
+		}
+		for (final String key : keysToRemove)
+			sounds.remove(key);
+		return allExist;
 	}
 }
