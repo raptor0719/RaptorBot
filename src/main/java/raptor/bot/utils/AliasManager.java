84015dcfd4eb38b830f93a93410d4da5f2b95f37
raptor0719 <shortman_gamer@hotmail.com>
2019-11-27 12:17:33 -0500

    Use properties format for alias file

--- src/main/java/raptor/bot/utils/AliasManager.java
+++ src/main/java/raptor/bot/utils/AliasManager.java
@@ -1,16 +1,15 @@
 package raptor.bot.utils;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import raptor.bot.api.IAliasManager;
 
@@ -58,33 +57,27 @@ public class AliasManager implements IAliasManager {
 
 	private Map<String, String> readAliasMappings(final String filePath) {
 		final File file = new File(filePath);
-		BufferedReader reader = null;
+
+		FileInputStream fis = null;
 		try {
+			final Properties props = new Properties();
+
+			fis = new FileInputStream(file);
+			props.load(fis);
+
 			final Map<String, String> aliasMap = new HashMap<String, String>();
-			if (!file.exists()) {
-				System.out.println("AliasManager - readAliasMappings - Alias file did not exist. Creating a new one.");
-				file.createNewFile();
-				return aliasMap;
-			}
-			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-			String current = reader.readLine();
-			while (current != null) {
-				final int firstSpace = current.indexOf(' ');
-				if (firstSpace+1 < current.length()) {
-					aliasMap.put(current.substring(0, firstSpace), current.substring(firstSpace+1, current.length()));
-				} else {
-					System.out.println("AliasManager - readAliasMappings - Invalid alias entry was encountered.");
-				}
-				current = reader.readLine();
-			}
+			for (final Map.Entry<Object, Object> e : props.entrySet())
+				aliasMap.put((String)e.getKey(), (String)e.getValue());
+
 			return aliasMap;
 		} catch (Throwable t) {
 			throw new RuntimeException(t);
 		} finally {
 			try {
-				if (reader != null)
-					reader.close();
-			} catch (Throwable t) {}
+				fis.close();
+			} catch (Throwable t) {
+				/* IGNORE FAILURES */
+			}
 		}
 	}
 
@@ -98,7 +91,7 @@ public class AliasManager implements IAliasManager {
 				throw new IOException("AliasManager - readAliasMappings - Alias mapping file could not be created.");
 			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
 			for (final Map.Entry<String, String> e : aliasMap.entrySet()) {
-				writer.write(e.getKey() + " " + e.getValue() + "\n");
+				writer.write(e.getKey() + " = " + e.getValue() + "\n");
 			}
 		} catch (Throwable t) {
 			throw new RuntimeException(t);
