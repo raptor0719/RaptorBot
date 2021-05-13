741250c73f93fb9fce6f339229cbc89972fcd9a5
raptor0719 <shortman_gamer@hotmail.com>
2021-05-13 00:03:51 -0400

    Update tts related classes to use new WavSoundBite as the transfer of audio data across api lines

--- src/main/java/raptor/bot/tts/PhonemeAudioManager.java
+++ src/main/java/raptor/bot/tts/PhonemeAudioManager.java
@@ -1,8 +1,12 @@
 package raptor.bot.tts;
 
+import java.util.List;
+
+import raptor.bot.utils.audio.WavSoundBite;
+
 // TODO: Fill this entire class in
 public class PhonemeAudioManager {
-	public byte[] getPhonemeAudio(final Phoneme phoneme) {
-		return new byte[] {};
+	public WavSoundBite getPhonemesAudio(final List<Phoneme> phonemes) {
+		return null;
 	}
 }
--- src/main/java/raptor/bot/tts/TextToSpeech.java
+++ src/main/java/raptor/bot/tts/TextToSpeech.java
@@ -2,7 +2,9 @@ package raptor.bot.tts;
 
 import java.util.List;
 
-//TODO: Fill this entire class in
+import raptor.bot.utils.audio.SoundPlayer;
+import raptor.bot.utils.audio.WavSoundBite;
+
 public class TextToSpeech {
 	private final PhonemeAudioManager audioManager;
 	private final PhonemeParser parser;
@@ -15,13 +17,11 @@ public class TextToSpeech {
 	public void speakPhrase(final String input) {
 		try {
 			final List<Phoneme> phonemes = parser.parse(input);
+			final WavSoundBite audio = audioManager.getPhonemesAudio(phonemes);
+			SoundPlayer.playSound(audio.getFormat(), audio.getData());
 		} catch (Exception e) {
 			System.err.println("Error attempting to text-to-speech phrase: " + input);
 			e.printStackTrace();
 		}
-	}
-
-	private byte[] getBytes(final List<Phoneme> phonemes) {
-		return new byte[] {};
 	}
 }
