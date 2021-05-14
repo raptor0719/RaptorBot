d9ed234f2ac241f3b264c6f81b3871949a33d60f
raptor0719 <shortman_gamer@hotmail.com>
2021-05-13 23:14:02 -0400

    Implement the PhonemeAudioManager

--- src/main/java/raptor/bot/tts/PhonemeAudioManager.java
+++ src/main/java/raptor/bot/tts/PhonemeAudioManager.java
@@ -1,12 +1,46 @@
 package raptor.bot.tts;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.sound.sampled.AudioFormat;
 
 import raptor.bot.utils.audio.WavSoundBite;
 
-// TODO: Fill this entire class in
 public class PhonemeAudioManager {
+	private final Map<Phoneme, byte[]> phonemeMap;
+	private final AudioFormat format;
+
+	public PhonemeAudioManager(final Map<Phoneme, byte[]> phonemeMap, final AudioFormat format) {
+		this.phonemeMap = phonemeMap;
+		this.format = format;
+	}
+
 	public WavSoundBite getPhonemesAudio(final List<Phoneme> phonemes) {
-		return null;
+		final List<byte[]> audio = new ArrayList<>();
+
+		for (final Phoneme ph : phonemes)
+			audio.add(phonemeMap.get(ph));
+
+		return new WavSoundBite(format, createByteArray(audio));
+	}
+
+	private byte[] createByteArray(final List<byte[]> bytes) {
+		int fullLength = 0;
+		for (final byte[] b : bytes)
+			fullLength += b.length;
+
+		final byte[] audio = new byte[fullLength];
+
+		int current = 0;
+		for (final byte[] arr : bytes) {
+			for (final byte b : arr) {
+				audio[current] = b;
+				current++;
+			}
+		}
+
+		return audio;
 	}
 }
